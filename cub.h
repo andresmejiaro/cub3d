186c7b0072a66b8197e71a3751e4467ed5f43788
Mateo Pizzolo <mpizzolo@c1r11s4.42madrid.com>
2023-06-10 20:11:29 +0200

    including math library

--- cub.h
+++ cub.h
@@ -6,7 +6,7 @@
 /*   By: mpizzolo <mpizzolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 18:59:17 by mpizzolo          #+#    #+#             */
-/*   Updated: 2023/06/10 20:06:23 by mpizzolo         ###   ########.fr       */
+/*   Updated: 2023/06/10 20:11:16 by mpizzolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <math.h>
 
 
 #endif
